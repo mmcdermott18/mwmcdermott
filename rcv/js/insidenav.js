8da9de51806f94837275832971f8432c659f2306
mmcdermott18 <mmcdermott18@gmail.com>
2015-12-31 00:00:47 -0500

    Lots!

---
+++ rcv/js/insidenav.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+		var v1 = $('#insideHead').height();
+	  	$(window).scroll(function () {
+	      //if you hard code, then use console
+	      //.log to determine when you want the 
+	      //nav bar to stick.  
+	      	console.log(v1)
+	     	console.log($('#navbar').outerHeight(true))
+    		if ($(window).scrollTop() > v1) {
+      			$('#navbar').addClass('navbar-fixed');
+      			$('#content').addClass('navbar-fix')
+    		}
+    		if ($(window).scrollTop() < (v1+1)) {
+      			$('#navbar').removeClass('navbar-fixed');
+      			$('#content').removeClass('navbar-fix')
+    		}
+	  	});
+	});
